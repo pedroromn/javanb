e73b2d20f84e23963275358d438536af19186b51
pedroromn <pedroromn@gmail.com>
2016-10-26 19:36:32 -0500

    26/10/2016 19:36

--- src/jvm/work/bourne/Time2.java
+++ src/jvm/work/bourne/Time2.java
@@ -8,8 +8,15 @@ package jvm.work.bourne;
 /**
  *
  * @author pyuser
+ * 
+ * Time2 class declaration with overloaded constructors
  */
 public class Time2 {
+    
+    private int hour; // 0 - 23 
+    private int minute; // 0 - 59
+    private int second; // 0 - 59
+    
     
     
 }
